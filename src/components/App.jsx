de8c6f54e71c26ecae03b846d9d1079237d07755
oLORDer <justsendmeow@gmail.com>
2022-08-12 22:23:27 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import Header from './Header/Header';
+
+export default function App() {
+  return <Header />;
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import s from './header.module.scss';
+
+export default function Header() {
+  return (
+    <ul className={s.list}>
+      <li>hihi</li>
+    </ul>
+  );
+}
